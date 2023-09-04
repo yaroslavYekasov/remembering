92f8cbef86b99f2663ac1087c2107223261ae642
yaroslavYekasov <120181426+yaroslavYekasov@users.noreply.github.com>
2023-09-04 11:33:33 +0300

    Add files via upload

---
+++ protseduurid.sql
@@ -0,0 +1,31 @@
+--lisamine ja SELECT
+
+create procedure lisaGuest
+@fname varchar(100),
+@lname varchar(100),
+@sinse date
+as
+begin
+insert into guest(first_name, last_name, member_since)
+values(@fname, @lname, @sinse);
+select * from guest;
+select * from logi;
+end;
+
+exec lisaguest 'Denis', 'Gorjunov', '2002-10-9';
+
+--kustutamine ja SELECT
+
+create procedure kustutaGuest
+@ID int
+as
+begin
+select * from guest;
+delete from guest
+where @id=id;
+select * from guest;
+select * from logi;
+end;
+
+exec kustutaGuest 3;
+
