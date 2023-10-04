929325aa9af37f81853fd589042def1133f82198
yaroslavYekasov <120181426+yaroslavYekasov@users.noreply.github.com>
2023-10-04 12:03:45 +0300

    Update tabeli_loomine – koopia.sql

--- tabeli_loomine – koopia.sql
+++ tabeli_loomine – koopia.sql
@@ -1,4 +1,4 @@
-create table quest (
+create table guest (
 id int primary key identity(1,1),
 first_name varchar(20),
 last_name varchar(20) null,
@@ -15,7 +15,7 @@ id int primary key identity(1,1),
 date_in date,
 date_out date,
 made_by varchar(20),
-guest_id int FOREIGN KEY REFERENCES quest(id)
+guest_id int FOREIGN KEY REFERENCES guest(id)
 );
 
 create table reserved_room(
@@ -45,8 +45,6 @@ resservation_id int FOREIGN KEY REFERENCES reservation(id)
 
 create table hosted_at(
 id int primary key identity(1,1),
-guest_id int FOREIGN KEY REFERENCES quest(id),
+guest_id int FOREIGN KEY REFERENCES guest(id),
 occupied_room_id int FOREIGN KEY REFERENCES occupied_room(id)
 );
-
-select * from reservation
